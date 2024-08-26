d091882251f8b42b82d30c69395218150821d01c
delta-vision0 <navanathbhosle29@gmail.com>
2024-08-26 13:16:04 +0530

    Update tahir.cpp

--- tahir.cpp
+++ tahir.cpp
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
-void readFile("product.txt");
+void readFile(const char *filename);
 void insertion();
 void deletion(int);
 void search(int);
-void traverse();
+void traverse(int);
+void saveFile(const char *filename);
 
 struct product{
 	int product_id;
-	char product_name;
+	char product_name[30];
 	float product_price;
 	float gst;
 };
@@ -30,48 +31,65 @@ int main()
 		{
 			case 1:
 				    insertion();
+				    break;
 			case 2:
-				    printf("Enter position to delete:");
+				    printf("\nEnter position to delete:");
 				    scanf("%d",&pos);
 				    pos-=1;
 				    deletion(pos);
+				    break;
 		    case 3:
-		    	    printf("Enter product ID delete:");
+		    	    printf("\nEnter product ID to search:");
 				    scanf("%d",&id);
 				    search(id);
+				    break;
 			case 4:
-					traverse();
+					traverse(count);
+				    break;
+			case 5:
+					saveFile("product.txt");
+					break;
+			default :
+					printf("Invalid Choice\n");		
 		}
 	}
 }
 
-void readFile("product.txt")
+void readFile(const char *filename)
 {
 	FILE *f1;
-	*f1=fopen("product.txt","r");
+	f1=fopen("product.txt","r");
+	if(f1==NULL)
+	{
+		printf("FIle not open\n");
+	}
 	for(count=0;count<100;count++)
 	{
-		if(fscanf("%d %s %f %f",&product_list[count].product_id,product_list[count].product_name,
-		                        &product_list[count].product_price,&product_list[count].gst)!=4)
+		if(fscanf(f1,"%d %s %f %f",&product_list[count].product_id,
+									product_list[count].product_name,
+		                        	&product_list[count].product_price,
+									&product_list[count].gst)!=4)
 		{
 			break;
 		} 
 	}
 	fclose(f1);
+	printf("count=%d\n",count);
 }
 
 void insertion()
 {
 	if(count<100)
 	{
-	printf("Enter product ID:");
+	printf("\nEnter product ID   :");
 	scanf("%d",&product_list[count].product_id);
-	printf("Enter product ID:");
+	printf("Enter product Name :");
 	scanf("%s",product_list[count].product_name);
-	printf("Enter product ID:");
+	printf("Enter product Price:");
 	scanf("%f",&product_list[count].product_price);
-	printf("Enter product ID:");
+	printf("Enter product GST  :");
 	scanf("%f",&product_list[count].gst);
+	count++;
     }
 	else
 		printf("List is full\n");
@@ -110,13 +128,42 @@ void search(int id)
 	}
 }
 
-void traverse()
+void traverse(int count)
 {
 	int i;
-	for(i=0;i,i<count;i++)
+	if(count>0)
 	{
-		printf("PRODUCT %d\n",i+1);
-		printf("Id= %d\nName= %s\nPrice= %f\ndGST= %f\n",product_list[i].product_id,product_list[i].product_name,
+		for(i=0;i,i<count;i++)
+		{
+			printf("\nPRODUCT %d\n",i+1);
+			printf("Id= %d\nName= %s\nPrice= %f\ndGST= %f\n",product_list[i].product_id,product_list[i].product_name,
 		                                                      product_list[i].product_price,product_list[i].gst);
+		}
 	}
+	else
+		printf("\nList is Empty\n");
 }
+
+void saveFile(const char *filename)
+{
+	int i;
+	FILE *f1;
+	f1=fopen("product.txt","w");
+	if(f1==NULL)
+	{
+		printf("FIle not open\n");
+	}
+	for(i=0;i<count;i++)
+	{
+	    fprintf(f1,"\n%d     %-10s     %-10.2f     %-10.2f",product_list[i].product_id,product_list[i].product_name,
+		                        product_list[i].product_price,product_list[i].gst);
+		
+	}	
+	fclose(f1);
+	printf("\nData save in file successfully\n");
+	printf("count=%d\n",count);
+}
+
+
+
+
